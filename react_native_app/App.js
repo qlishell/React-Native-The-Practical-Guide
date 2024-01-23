253a4f5ec11f6d71d6b799b061305d51b9ec4270
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2024-01-23 13:19:32 +0700

    NFT Marketplace

---
+++ react_native_app/App.js
@@ -0,0 +1,43 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+
+import { useFonts } from "expo-font";
+
+import Home from "./screens/Home";
+import Details from "./screens/Details";
+
+const Stack = createStackNavigator();
+
+const theme = {
+    ...DefaultTheme,
+    color: {
+        ...DefaultTheme.colors,
+        background: "transparent",
+    },
+};
+
+const App = () => {
+    const [loaded] = useFonts({
+        InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+        InterLight: require("./assets/fonts/Inter-Light.ttf"),
+        InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+        InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+        InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    });
+
+    if (!loaded) return null;
+
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                screenOptions={{ headerShown: false }}
+                initialRouteName="Home"
+            >
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Details" component={Details} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default App;
---
+++ react_native_app/README.md
@@ -0,0 +1,3 @@
+# NFT Marketplace
+
+Master React Native by building a modern NFT Marketplace iOS and Android **ReactNative** Application in one video! Alongside developing the application, you'll create a great landing page to showcase all of your app's features!
