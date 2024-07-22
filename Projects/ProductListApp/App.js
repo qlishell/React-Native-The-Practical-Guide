cadef3e7e899ccfc2067b6cb14bcc3471cfeffeb
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-22 14:02:37 -0500

    refactor(dependencies): remove unused dependencies

--- Projects/ProductListApp/App.js
+++ Projects/ProductListApp/App.js
@@ -1,12 +1,34 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import "react-native-gesture-handler";
+import { COLORS } from "./app/constants";
 import AppNavigator from "./app/navigation/AppNavigator";
 
+const theme = {
+    ...DefaultTheme,
+    color: {
+        ...DefaultTheme.colors,
+        background: "transparent",
+    },
+};
+
 export default function App() {
+    const [loaded] = useFonts({
+        InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+        InterLight: require("./assets/fonts/Inter-Light.ttf"),
+        InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+        InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+        InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    });
+
+    if (!loaded) return null;
+
     return (
-        <NavigationContainer>
-            <AppNavigator />
-        </NavigationContainer>
+        <LinearGradient colors={[COLORS.primary, COLORS.accent500]} style={{ flex: 1 }}>
+            <NavigationContainer theme={theme}>
+                <AppNavigator />
+            </NavigationContainer>
+        </LinearGradient>
     );
 }
