bd0405ed5d1741cc6f763b9919731a9773dd38cc
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-22 13:56:38 -0500

    adding other primary

--- Projects/ProductListApp/app/constants/index.js
+++ Projects/ProductListApp/app/constants/index.js
@@ -4,7 +4,6 @@ import left from "../../assets/icons/left.png";
 import search from "../../assets/icons/search.png";
 import { products } from "../data/products";
 import { COLORS, FONTS, SHADOWS, SIZES } from "./theme";
-
 const assets = { badge, heart, left, search };
 
 export { assets, COLORS, FONTS, products, SHADOWS, SIZES };
--- Projects/ProductListApp/app/constants/theme.js
+++ Projects/ProductListApp/app/constants/theme.js
@@ -1,5 +1,10 @@
 export const COLORS = {
     primary: "#001F2D",
+    primary500: "#72063c",
+    primary600: "#640233",
+    primary700: "#4e0329",
+    primary800: "#3b021f",
+    accent500: "#ddb52f",
     secondary: "#4D626C",
     white: "#FFF",
     gray: "#74858C",
