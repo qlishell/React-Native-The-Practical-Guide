a3a0a4b504625808f372c53995bb96a43d45ee4b
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-22 08:46:14 -0500

    organize constants structure

---
+++ Projects/ProductListApp/app/constants/index.js
@@ -0,0 +1,10 @@
+import badge from "../../assets/icons/badge.png";
+import heart from "../../assets/icons/heart.png";
+import left from "../../assets/icons/left.png";
+import search from "../../assets/icons/search.png";
+import { products } from "../data/products";
+import { COLORS, FONTS, SHADOWS, SIZES } from "./theme";
+
+const assets = { badge, heart, left, search };
+
+export { assets, COLORS, FONTS, products, SHADOWS, SIZES };
--- Projects/ProductListApp/app/constants/theme.js
+++ Projects/ProductListApp/app/constants/theme.js
@@ -1,7 +1,6 @@
 export const COLORS = {
     primary: "#001F2D",
     secondary: "#4D626C",
-
     white: "#FFF",
     gray: "#74858C",
 };
