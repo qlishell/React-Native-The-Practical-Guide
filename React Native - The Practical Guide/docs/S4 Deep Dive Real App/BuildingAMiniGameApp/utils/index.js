b466446c0dcc7bece3d6d6ea524583b8c0b18b6e
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-23 15:50:38 -0500

    utils

--- React Native - The Practical Guide/docs/S4 Deep Dive Real App/BuildingAMiniGameApp/utils/index.js
+++ React Native - The Practical Guide/docs/S4 Deep Dive Real App/BuildingAMiniGameApp/utils/index.js
@@ -1,6 +1,5 @@
 export function generateRandomBetween(min, max, exclude) {
     const rndNum = Math.floor(Math.random() * (max - min)) + min;
-
     if (rndNum === exclude) {
         return generateRandomBetween(min, max, exclude);
     } else {
