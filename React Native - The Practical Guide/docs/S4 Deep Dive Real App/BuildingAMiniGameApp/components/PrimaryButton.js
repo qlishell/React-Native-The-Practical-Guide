8b2d8c4e947f32d72ef70527c8030b30e6baedd7
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-23 13:42:29 -0500

    PrimaryButton

--- React Native - The Practical Guide/docs/S4 Deep Dive Real App/BuildingAMiniGameApp/components/PrimaryButton.js
+++ React Native - The Practical Guide/docs/S4 Deep Dive Real App/BuildingAMiniGameApp/components/PrimaryButton.js
@@ -1,14 +1,45 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const PrimaryButton = ({ children }) => {
+    function pressHandler() {
+        console.log("Pressed!");
+    }
+
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                style={({ pressed }) =>
+                    pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer
+                }
+                onPress={pressHandler}
+                android_ripple={{ color: "#640233" }}
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: "hidden",
+    },
+    buttonInnerContainer: {
+        backgroundColor: "#72063c",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+    },
+    buttonText: {
+        color: "white",
+        textAlign: "center",
+    },
+    pressed: {
+        opacity: 0.75,
+    },
+});
 
 export default PrimaryButton;
