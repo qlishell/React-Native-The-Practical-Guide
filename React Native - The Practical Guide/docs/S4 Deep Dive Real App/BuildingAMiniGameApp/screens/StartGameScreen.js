8dc03a7e864c4734bd148cd9af23c409a5b1d6a3
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-23 13:42:48 -0500

    StartGameScreen

--- React Native - The Practical Guide/docs/S4 Deep Dive Real App/BuildingAMiniGameApp/screens/StartGameScreen.js
+++ React Native - The Practical Guide/docs/S4 Deep Dive Real App/BuildingAMiniGameApp/screens/StartGameScreen.js
@@ -1,14 +1,62 @@
 import React from "react";
 import { StyleSheet, TextInput, View } from "react-native";
 
+import PrimaryButton from "../components/PrimaryButton";
+
 const StartGameScreen = () => {
     return (
-        <View>
-            <TextInput />
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    inputContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 100,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: "#3b021f",
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: "#ddb52f",
+        borderBottomWidth: 2,
+        color: "#ddb52f",
+        marginVertical: 8,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    buttonsContainer: {
+        flexDirection: "row",
+    },
+    buttonContainer: {
+        flex: 1,
+    },
+});
 
 export default StartGameScreen;
