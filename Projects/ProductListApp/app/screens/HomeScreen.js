17e95ff16f76a18ef4efd662ec45ca32e5df7023
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-23 20:22:10 -0500

    SkeletonCard

--- Projects/ProductListApp/app/screens/HomeScreen.js
+++ Projects/ProductListApp/app/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, SafeAreaView, View } from "react-native";
 import ProductItem from "../components/ProductItem";
-import Skeleton from "../components/Skeleton";
+import SkeletonCard from "../components/SkeletonCard";
 import { products } from "../constants";
 
 const HomeScreen = ({ navigation }) => {
@@ -47,28 +47,7 @@ const HomeScreen = ({ navigation }) => {
         return (
             <>
                 {[...Array(5)].map((_, index) => (
-                    <View
-                        key={`skeleton-${index}`}
-                        style={{
-                            padding: 10,
-                            margin: 8,
-                            backgroundColor: "white",
-                            borderRadius: 8,
-                            shadowColor: "#000", // Shadow used iOS
-                            shadowOpacity: 0.1,
-                            shadowOffset: { width: 0, height: 2 },
-                            shadowRadius: 8,
-                            elevation: 5, // Elevation used Android
-                            overflow: "hidden",
-                        }}
-                    >
-                        <Skeleton width="100%" height={250} style={{ resizeMode: "cover", borderRadius: 8 }} />
-                        <View style={{ padding: 12, justifyContent: "center" }}>
-                            <Skeleton width="80%" height={20} style={{ marginBottom: 8 }} />
-                            <Skeleton width="40%" height={16} style={{ marginTop: 4 }} />
-                        </View>
-                        {/* <Skeleton width={100} height={40} style={{ position: "absolute", bottom: 8, right: 8 }} /> */}
-                    </View>
+                    <SkeletonCard index={index} />
                 ))}
             </>
         );
