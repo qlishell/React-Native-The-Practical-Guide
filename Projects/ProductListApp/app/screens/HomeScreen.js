0106ebc3c5a3b36833928eea9239431916f0b31b
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-23 21:37:02 -0500

    fix(data): update fetch product data from db

--- Projects/ProductListApp/app/screens/HomeScreen.js
+++ Projects/ProductListApp/app/screens/HomeScreen.js
@@ -2,36 +2,59 @@ import React, { useCallback, useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, SafeAreaView, View } from "react-native";
 import ProductItem from "../components/ProductItem";
 import SkeletonCard from "../components/SkeletonCard";
-import { products } from "../constants";
+import { getProducts } from "../database/db";
 
 const HomeScreen = ({ navigation }) => {
     const [displayedProducts, setDisplayedProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [allProducts, setAllProducts] = useState([]);
+    const [page, setPage] = useState(0);
+    const itemsPerPage = 5;
 
+    // Sử dụng useEffect để fetch dữ liệu sản phẩm từ cơ sở dữ liệu khi component mount.
     useEffect(() => {
-        // Simulate initial loading delay
-        setTimeout(() => {
-            setDisplayedProducts(products.slice(0, 5));
-            setIsLoading(false);
-        }, 3000);
+        const fetchProducts = async () => {
+            try {
+                const products = await getProducts();
+                setAllProducts(products);
+                loadInitialProducts(products);
+            } catch (error) {
+                console.error("Error fetching products:", error);
+                setIsLoading(false);
+            }
+        };
+
+        fetchProducts();
     }, []);
 
+    const loadInitialProducts = products => {
+        setTimeout(() => {
+            setDisplayedProducts(products.slice(0, itemsPerPage));
+            setIsLoading(false);
+            setPage(1);
+        }, 3000); // Simulating initial loading delay
+    };
+
     const renderItem = useCallback(
-        ({ item }) => <ProductItem item={item} onPress={() => navigation.navigate("Detail", { product: item })} />,
+        ({ item }) => <ProductItem item={item} onPress={() => navigation.navigate("Detail", { productId: item.id })} />,
         [navigation],
     );
 
-    const keyExtractor = useCallback(item => item.id, []);
+    const keyExtractor = useCallback(item => item.id.toString(), []);
 
     const loadMoreItems = () => {
-        if (isLoading || displayedProducts.length >= products.length) return;
+        if (isLoading || displayedProducts.length >= allProducts.length) return;
 
         setIsLoading(true);
         setTimeout(() => {
-            const newProducts = products.slice(displayedProducts.length, displayedProducts.length + 5);
+            const nextPage = page + 1;
+            const startIndex = page * itemsPerPage;
+            const endIndex = startIndex + itemsPerPage;
+            const newProducts = allProducts.slice(startIndex, endIndex);
             setDisplayedProducts(prevProducts => [...prevProducts, ...newProducts]);
             setIsLoading(false);
-        }, 3000); // Simulating network delay
+            setPage(nextPage);
+        }, 1000); // Simulating network delay
     };
 
     const renderFooter = () => {
@@ -45,11 +68,13 @@ const HomeScreen = ({ navigation }) => {
 
     if (displayedProducts.length === 0) {
         return (
-            <>
-                {[...Array(5)].map((_, index) => (
-                    <SkeletonCard index={index} />
-                ))}
-            </>
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={{ flex: 1, backgroundColor: "#f8f8f8", padding: 8 }}>
+                    {[...Array(5)].map((_, index) => (
+                        <SkeletonCard key={index} />
+                    ))}
+                </View>
+            </SafeAreaView>
         );
     }
 
@@ -62,7 +87,7 @@ const HomeScreen = ({ navigation }) => {
                     keyExtractor={keyExtractor}
                     numColumns={1}
                     contentContainerStyle={{ padding: 8 }}
-                    initialNumToRender={5}
+                    initialNumToRender={itemsPerPage}
                     onEndReached={loadMoreItems}
                     onEndReachedThreshold={0.1}
                     ListFooterComponent={renderFooter}
