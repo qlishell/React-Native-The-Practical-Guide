4079f5e9af9f978483d15deb5f615022b3d26e64
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-25 08:27:39 -0500

    Create new InstructionText

---
+++ React Native - The Practical Guide/docs/S4 Deep Dive Real App/BuildingAMiniGameApp/components/ui/InstructionText.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../constants";
+
+const InstructionText = ({ children, style }) => {
+    return <Text style={[styles.instructionText, style]}>{children}</Text>;
+};
+
+const styles = StyleSheet.create({
+    instructionText: {
+        color: COLORS.accent500,
+        fontSize: 24,
+    },
+});
+
+export default InstructionText;
