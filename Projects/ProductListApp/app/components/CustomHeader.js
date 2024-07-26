1164849518b7cdb230be390082de0a62d33ea7bc
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-26 15:32:43 -0500

    Adding navigate Cart

--- Projects/ProductListApp/app/components/CustomHeader.js
+++ Projects/ProductListApp/app/components/CustomHeader.js
@@ -1,15 +1,20 @@
 import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const CustomHeader = () => (
-    <View style={styles.header}>
-        <Text style={styles.headerTitle}>Danh sách sản phẩm</Text>
-        <TouchableOpacity style={styles.cartButton}>
-            <Ionicons name="cart-outline" size={24} color="#333" />
-        </TouchableOpacity>
-    </View>
-);
+const CustomHeader = () => {
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>Danh sách sản phẩm</Text>
+            <TouchableOpacity style={{ padding: 2 }} onPress={() => navigation.navigate("Cart")}>
+                <Ionicons name="cart-outline" size={24} color="#333" />
+            </TouchableOpacity>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     header: {
@@ -26,9 +31,6 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: "bold",
         color: "#333",
-    },
-    cartButton: {
-        padding: 2,
     },
 });
 
