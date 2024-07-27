d47a5ba215a603a05470028b73edb6781f722cfd
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-27 17:17:32 -0500

    Adding new CART_KEY

--- Projects/ProductListApp/app/config/databases/AsyncStorageDB.js
+++ Projects/ProductListApp/app/config/databases/AsyncStorageDB.js
@@ -2,9 +2,14 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { products } from "../constants";
 
 const PRODUCTS_KEY = "@products";
+const CART_KEY = "cart";
 
 export const initDatabase = async () => {
     try {
+        AsyncStorage.removeItem(CART_KEY)
+            .then(() => console.log("Item CART_KEY removed"))
+            .catch(error => console.error("Failed to remove item:", error));
+
         const productsJSON = await AsyncStorage.getItem(PRODUCTS_KEY);
         if (productsJSON === null) {
             // Initialize with empty array if no products exist
