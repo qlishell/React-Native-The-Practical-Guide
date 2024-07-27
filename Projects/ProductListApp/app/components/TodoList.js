c7fb698b28e303f3eda558cd640c7aed1535d5d5
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-27 17:14:38 -0500

    fix(styles)

--- Projects/ProductListApp/app/components/TodoList.js
+++ Projects/ProductListApp/app/components/TodoList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { COLORS, SIZES } from "../constants";
 import Card from "./ui/Card";
 
 const TodoList = ({ todos }) => {
     return (
         <View style={styles.container}>
             {todos.map((todo, index) => (
-                <Card key={index}>
-                    <Text style={styles.todoText}>{`${todo.intValue} - ${todo.value}`}</Text>
+                <Card key={index} style={{ margin: 2 }}>
+                    <Text style={styles.todoText}>{`${todo.intValue} 🌟 ${todo.value}`}</Text>
                 </Card>
             ))}
         </View>
@@ -17,12 +18,12 @@ const TodoList = ({ todos }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
-        backgroundColor: "#f5f5f5",
+        padding: SIZES.base,
+        backgroundColor: COLORS.primary200,
     },
     todoText: {
-        fontSize: 16,
-        color: "#333",
+        fontSize: SIZES.font,
+        color: COLORS.primary900,
     },
 });
 
