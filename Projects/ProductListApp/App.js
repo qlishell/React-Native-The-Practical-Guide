7aeb8100c58e083acc917f1641fc9ee31966f3c3
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-27 17:13:24 -0500

    refactor(deps): SQLite

--- Projects/ProductListApp/App.js
+++ Projects/ProductListApp/App.js
@@ -1,12 +1,10 @@
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { LinearGradient } from "expo-linear-gradient";
 import { SQLiteProvider } from "expo-sqlite";
-import React, { useEffect } from "react";
-import { AppRegistry } from "react-native";
-import { name as appName } from "./app.json";
-import { initDatabase, insertSampleData } from "./app/config/databases/SQLiteDB";
-import { COLORS } from "./app/constants";
+import React, { useRef } from "react";
+import Toast from "react-native-toast-message";
+import { initDatabase } from "./app/config/databases/SQLiteDB";
+import { CartProvider } from "./app/context/CartContext";
 import AppNavigator from "./app/navigation/AppNavigator";
 
 const theme = {
@@ -18,6 +16,8 @@ const theme = {
 };
 
 export default function App() {
+    const toastRef = useRef(null);
+
     const [loaded] = useFonts({
         InterBold: require("./assets/fonts/Inter-Bold.ttf"),
         InterLight: require("./assets/fonts/Inter-Light.ttf"),
@@ -26,52 +26,40 @@ export default function App() {
         InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
     });
 
-    useEffect(() => {
-        const setupDatabase = async () => {
-            try {
-                await initDatabase();
-                await insertSampleData();
-                console.log("Database setup completed");
-            } catch (error) {
-                console.error("Error setting up database:", error);
-            }
-        };
-
-        setupDatabase();
-    }, []);
-
     if (!loaded) return null;
 
     return (
-        <LinearGradient colors={[COLORS.primary500, COLORS.accent500]} style={{ flex: 1 }}>
-            <SQLiteProvider databaseName="test.db" onInit={migrateDbIfNeeded}>
+        <SQLiteProvider databaseName="productDB.db" onInit={migrateDbIfNeeded}>
+            <CartProvider>
                 <NavigationContainer theme={theme}>
                     <AppNavigator />
                 </NavigationContainer>
-            </SQLiteProvider>
-        </LinearGradient>
+                <Toast ref={toastRef} />
+            </CartProvider>
+        </SQLiteProvider>
     );
 }
 
 async function migrateDbIfNeeded(db) {
     const DATABASE_VERSION = 1;
-    let { user_version: currentDbVersion } = await db.getFirstAsync("PRAGMA user_version");
-    if (currentDbVersion >= DATABASE_VERSION) {
-        return;
-    }
-    if (currentDbVersion === 0) {
-        await db.execAsync(`
-            PRAGMA journal_mode = 'wal';
-            CREATE TABLE todos (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
-        `);
-        await db.runAsync("INSERT INTO todos (value, intValue) VALUES (?, ?)", "hello", 1);
-        await db.runAsync("INSERT INTO todos (value, intValue) VALUES (?, ?)", "world", 2);
-        currentDbVersion = 1;
-    }
-    // if (currentDbVersion === 1) {
-    //   Add more migrations
-    // }
-    await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`);
-}
+    try {
+        // await clearDbVersion();
+        let { user_version: currentDbVersion } = await db.getFirstAsync("PRAGMA user_version");
+        console.log(`DatabaseName: ${db.databaseName} - Version: ${currentDbVersion}`);
 
-AppRegistry.registerComponent(appName, () => App);
+        if (currentDbVersion >= DATABASE_VERSION) {
+            return;
+        }
+        if (currentDbVersion === 0) {
+            await initDatabase(db);
+            currentDbVersion = 1;
+        }
+        // if (currentDbVersion === 1) {
+        //     // Add more migrations
+        // }
+        await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`);
+        console.log("Database setup completed");
+    } catch (error) {
+        console.error("Error setting up database:", error);
+    }
+}
