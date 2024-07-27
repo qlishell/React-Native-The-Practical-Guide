260accc0188c00cd569dbb840dddb35556569792
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-27 17:09:50 -0500

    Create new FocusedStatusBar

---
+++ Projects/ProductListApp/app/components/FocusedStatusBar.js
@@ -0,0 +1,9 @@
+import { useIsFocused } from "@react-navigation/native";
+import { StatusBar } from "react-native";
+
+const FocusedStatusBar = props => {
+    const isFocused = useIsFocused();
+    return isFocused ? <StatusBar animated={true} {...props} /> : null;
+};
+
+export default FocusedStatusBar;
