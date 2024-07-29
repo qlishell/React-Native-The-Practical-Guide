dddd10d5df84caae0b8820fb089247e8abee6d99
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-29 13:14:07 -0500

    styling game round logs

--- React Native - The Practical Guide/docs/S4 Deep Dive Real App/BuildingAMiniGameApp/screens/GameOverScreen.js
+++ React Native - The Practical Guide/docs/S4 Deep Dive Real App/BuildingAMiniGameApp/screens/GameOverScreen.js
@@ -1,8 +1,54 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, Image, Text, StyleSheet } from 'react-native';
+import {Title, PrimaryButton} from '../components';
+import { COLORS } from "../constants";
 
 const GameOverScreen = () => {
-    return <Text>Game is over!</Text>;
+    return (
+        <View style={styles.rootContainer}>
+            <Title>GAME OVER!</Title>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={require("../assets/images/success.png")} />
+            </View>
+            <Text style={styles.summaryText}>
+                Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess the number{" "}
+                <Text style={styles.highlight}>{userNumber}</Text>.
+            </Text>
+            <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: COLORS.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  summaryText: {
+    fontFamily: 'open-sans',
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: 'open-sans-bold',
+    color: COLORS.primary500,
+  },
+});
 
 export default GameOverScreen;
