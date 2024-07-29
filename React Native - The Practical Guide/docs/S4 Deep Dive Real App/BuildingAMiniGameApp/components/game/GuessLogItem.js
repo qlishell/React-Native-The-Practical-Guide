a8228c9200a18ae98d818c9e3dbd363fe3e66735
Qliphort Shell <nhoangkiet35@gmail.com>
2024-07-29 13:13:21 -0500

    Create new GuessLogItem

---
+++ React Native - The Practical Guide/docs/S4 Deep Dive Real App/BuildingAMiniGameApp/components/game/GuessLogItem.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import COLORS from "../../constants";
+
+function GuessLogItem({ roundNumber, guess }) {
+    return (
+        <View style={styles.listItem}>
+            <Text style={styles.itemText}>#{roundNumber}</Text>
+            <Text style={styles.itemText}>Opponent's Guess: {guess}</Text>
+        </View>
+    );
+}
+
+export default GuessLogItem;
+
+const styles = StyleSheet.create({
+    listItem: {
+        borderColor: COLORS.primary800,
+        borderWidth: 1,
+        borderRadius: 40,
+        padding: 12,
+        marginVertical: 8,
+        backgroundColor: COLORS.accent500,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+        elevation: 4,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3,
+    },
+    itemText: {
+        fontFamily: "open-sans",
+    },
+});
