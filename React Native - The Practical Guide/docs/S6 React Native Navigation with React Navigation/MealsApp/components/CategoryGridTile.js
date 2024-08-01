19859c2ccae91f9a516e6b527d8dfcbf612c0e72
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-01 15:24:07 -0500

    adding onPress event

--- React Native - The Practical Guide/docs/S6 React Native Navigation with React Navigation/MealsApp/components/CategoryGridTile.js
+++ React Native - The Practical Guide/docs/S6 React Native Navigation with React Navigation/MealsApp/components/CategoryGridTile.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
 
-const CategoryGridTile = ({ title, color }) => {
+const CategoryGridTile = ({ title, color, onPress }) => {
     return (
         <View style={styles.gridItem}>
             <Pressable
                 android_ripple={{ color: "#ccc" }}
                 style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}
+                onPress={onPress}
             >
                 <View style={[styles.innerContainer, { backgroundColor: color }]}>
                     <Text style={styles.title}>{title}</Text>
