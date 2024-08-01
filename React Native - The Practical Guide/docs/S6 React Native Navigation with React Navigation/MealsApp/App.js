a62233f982dbb33a2ceb0efe810c1825e40d6915
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-01 15:25:08 -0500

    adding Stack Navigation

--- React Native - The Practical Guide/docs/S6 React Native Navigation with React Navigation/MealsApp/App.js
+++ React Native - The Practical Guide/docs/S6 React Native Navigation with React Navigation/MealsApp/App.js
@@ -1,12 +1,46 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
+
 import CategoriesScreen from "./screens/CategoriesScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <>
             <StatusBar style="light" />
-            <CategoriesScreen />
+            <NavigationContainer>
+                <Stack.Navigator
+                    screenOptions={{
+                        headerStyle: { backgroundColor: "#351401" },
+                        headerTintColor: "white",
+                        contentStyle: { backgroundColor: "#3f2f25" },
+                    }}
+                >
+                    <Stack.Screen
+                        name="MealsCategories"
+                        component={CategoriesScreen}
+                        options={{
+                            title: "All Categories",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="MealsOverview"
+                        component={MealsOverviewScreen}
+                        // options={({ route, navigation }) => {
+                        //   const catId = route.params.categoryId;
+                        //   return {
+                        //     title: catId,
+                        //   };
+                        // }}
+                    />
+                    <Stack.Screen name="MealDetail" component={MealDetailScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
         </>
     );
 }
