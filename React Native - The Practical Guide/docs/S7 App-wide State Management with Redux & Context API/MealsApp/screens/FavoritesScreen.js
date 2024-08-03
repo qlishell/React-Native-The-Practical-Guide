1f78f385e34f281c38d94a69759325af07f9f009
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-03 17:47:41 -0500

    managing redux state

--- React Native - The Practical Guide/docs/S7 App-wide State Management with Redux & Context API/MealsApp/screens/FavoritesScreen.js
+++ React Native - The Practical Guide/docs/S7 App-wide State Management with Redux & Context API/MealsApp/screens/FavoritesScreen.js
@@ -1,14 +1,16 @@
-import React, { useContext } from "react";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
 
 import MealsList from "../components/MealsList/MealsList";
 import { MEALS } from "../data/dummy-data";
-import { FavoritesContext } from "../store/context/favorites-context";
+// import { FavoritesContext } from "../store/context/favorites-context";
 
 const FavoritesScreen = () => {
-    const favoriteMealsCtx = useContext(FavoritesContext);
+    // const favoriteMealsCtx = useContext(FavoritesContext);
+    const favoriteMealIds = useSelector(state => state.favoriteMeals.ids);
 
-    const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id));
+    const favoriteMeals = MEALS.filter(meal => favoriteMealIds.includes(meal.id));
 
     if (favoriteMeals.length === 0) {
         return (
