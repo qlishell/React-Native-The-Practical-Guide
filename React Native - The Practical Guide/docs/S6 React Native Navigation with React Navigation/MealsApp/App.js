cd58fa3f0c34d07a1e0c43938692e5aede3d1a98
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-03 11:01:35 -0500

    Drawer customize

--- React Native - The Practical Guide/docs/S6 React Native Navigation with React Navigation/MealsApp/App.js
+++ React Native - The Practical Guide/docs/S6 React Native Navigation with React Navigation/MealsApp/App.js
@@ -1,3 +1,4 @@
+import { Ionicons } from "@expo/vector-icons";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -19,6 +20,10 @@ function DrawerNavigator() {
                 headerStyle: { backgroundColor: "#351401" },
                 headerTintColor: "white",
                 sceneContainerStyle: { backgroundColor: "#3f2f25" },
+                drawerContentStyle: { backgroundColor: "#351401" },
+                drawerInactiveTintColor: "white",
+                drawerActiveTintColor: "#351401",
+                drawerActiveBackgroundColor: "#e4baa1",
             }}
         >
             <Drawer.Screen
@@ -26,9 +31,16 @@ function DrawerNavigator() {
                 component={CategoriesScreen}
                 options={{
                     title: "All Categories",
+                    drawerIcon: ({ color, size }) => <Ionicons name="list" color={color} size={size} />,
                 }}
             />
-            <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+            <Drawer.Screen
+                name="Favorites"
+                component={FavoritesScreen}
+                options={{
+                    drawerIcon: ({ color, size }) => <Ionicons name="star" color={color} size={size} />,
+                }}
+            />
         </Drawer.Navigator>
     );
 }
