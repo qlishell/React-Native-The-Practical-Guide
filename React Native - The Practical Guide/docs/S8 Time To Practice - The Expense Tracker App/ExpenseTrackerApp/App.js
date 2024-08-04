560038270d1971d2a17d0251b44214f94c53f23a
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-03 20:24:28 -0500

    adding navigation

--- React Native - The Practical Guide/docs/S8 Time To Practice - The Expense Tracker App/ExpenseTrackerApp/App.js
+++ React Native - The Practical Guide/docs/S8 Time To Practice - The Expense Tracker App/ExpenseTrackerApp/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import AllExpenses from "./screens/AllExpenses";
+import ManageExpense from "./screens/ManageExpense";
+import RecentExpenses from "./screens/RecentExpenses";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+    return (
+        <BottomTabs.Navigator>
+            <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+            <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+        </BottomTabs.Navigator>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+    return (
+        <>
+            <StatusBar style="auto" />
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+                    <Stack.Screen name="ManageExpense" component={ManageExpense} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({});
