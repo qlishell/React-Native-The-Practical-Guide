22299e3a75fc02d12f21cb6a7eca000634d6067c
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-06 13:08:17 -0500

    adding a header button

--- React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/App.js
+++ React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 
+import IconButton from "./components/UI/IconButton";
 import AddPlace from "./screens/AddPlace";
 import AllPlaces from "./screens/AllPlaces";
 
@@ -13,7 +14,20 @@ export default function App() {
             <StatusBar style="dark" />
             <NavigationContainer>
                 <Stack.Navigator>
-                    <Stack.Screen name="AllPlaces" component={AllPlaces} />
+                    <Stack.Screen
+                        name="AllPlaces"
+                        component={AllPlaces}
+                        options={({ navigation }) => ({
+                            headerRight: ({ tintColor }) => (
+                                <IconButton
+                                    icon="add"
+                                    size={24}
+                                    color={tintColor}
+                                    onPress={() => navigation.navigate("AddPlace")}
+                                />
+                            ),
+                        })}
+                    />
                     <Stack.Screen name="AddPlace" component={AddPlace} />
                 </Stack.Navigator>
             </NavigationContainer>
