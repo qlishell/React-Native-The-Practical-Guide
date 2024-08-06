ba5d1149b0e8144131a1dc1f26a3c16328e2c380
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-05 22:10:46 -0500

    protecting screens

--- React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/App.js
+++ React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/App.js
@@ -1,12 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
+import { useContext } from "react";
 
 import { Colors } from "./constants/styles";
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
-import AuthContextProvider from "./store/auth-context";
+import AuthContextProvider, { AuthContext } from "./store/auth-context";
 
 const Stack = createNativeStackNavigator();
 
@@ -40,12 +41,13 @@ function AuthenticatedStack() {
 }
 
 function Navigation() {
+    const authCtx = useContext(AuthContext);
+
     return (
-        <AuthContextProvider>
-            <NavigationContainer>
-                <AuthStack />
-            </NavigationContainer>
-        </AuthContextProvider>
+        <NavigationContainer>
+            {!authCtx.isAuthenticated && <AuthStack />}
+            {authCtx.isAuthenticated && <AuthenticatedStack />}
+        </NavigationContainer>
     );
 }
 
@@ -53,7 +55,9 @@ export default function App() {
     return (
         <>
             <StatusBar style="light" />
-            <Navigation />
+            <AuthContextProvider>
+                <Navigation />
+            </AuthContextProvider>
         </>
     );
 }
