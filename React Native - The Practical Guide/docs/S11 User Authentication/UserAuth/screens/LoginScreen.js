7c3400735267367e6216f9f165e92f7632bccc80
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-05 21:06:08 -0500

    logging users in

--- React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/screens/LoginScreen.js
+++ React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/screens/LoginScreen.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../util/auth";
 
 function LoginScreen() {
-    return <AuthContent isLogin />;
+    const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+    async function loginHandler({ email, password }) {
+        setIsAuthenticating(true);
+        await login(email, password);
+        setIsAuthenticating(false);
+    }
+
+    if (isAuthenticating) {
+        return <LoadingOverlay message="Logging you in..." />;
+    }
+
+    return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/util/auth.js
+++ React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/util/auth.js
@@ -2,10 +2,22 @@ import axios from "axios";
 
 const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
 
-export async function createUser(email, password) {
-    const response = await axios.post(process.env.EXPO_PUBLIC_API_URL + API_KEY, {
+async function authenticate(mode, email, password) {
+    const url = `${process.env.EXPO_PUBLIC_API_URL}${mode}?key=${API_KEY}`;
+
+    const response = await axios.post(url, {
         email: email,
         password: password,
         returnSecureToken: true,
     });
+
+    console.log(response.data);
+}
+
+export async function createUser(email, password) {
+    await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+    await authenticate("signInWithPassword", email, password);
 }
