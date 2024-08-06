9ab03ac1b013a2bf4f12dc6de7eceb088c559a1c
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-06 18:14:15 -0500

    outputting a list of places

--- React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/components/Places/PlaceForm.js
+++ React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/components/Places/PlaceForm.js
@@ -2,11 +2,12 @@ import { useCallback, useState } from "react";
 import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 import { Colors } from "../../constants/colors";
+import { Place } from "../../models/place";
 import Button from "../UI/Button";
 import ImagePicker from "./ImagePicker";
 import LocationPicker from "./LocationPicker";
 
-function PlaceForm() {
+function PlaceForm({ onCreatePlace }) {
     const [enteredTitle, setEnteredTitle] = useState("");
     const [selectedImage, setSelectedImage] = useState();
     const [pickedLocation, setPickedLocation] = useState();
@@ -24,9 +25,8 @@ function PlaceForm() {
     }, []);
 
     function savePlaceHandler() {
-        console.log(enteredTitle);
-        console.log(selectedImage);
-        console.log(pickedLocation);
+        const placeData = new Place(enteredTitle, selectedImage, pickedLocation);
+        onCreatePlace(placeData);
     }
 
     return (
