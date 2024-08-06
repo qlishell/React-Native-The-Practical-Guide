bdd8eb9275e295ebbab85a0bdaad9b4aab5edf5c
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-05 23:17:34 -0500

    storing auth tokens on the device

--- React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/App.js
+++ React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/App.js
@@ -1,7 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import AppLoading from "expo-app-loading";
 
 import IconButton from "./components/ui/IconButton";
 import { Colors } from "./constants/styles";
@@ -61,13 +63,40 @@ function Navigation() {
     );
 }
 
+
+function Root() {
+    const [isTryingLogin, setIsTryingLogin] = useState(true);
+
+    const authCtx = useContext(AuthContext);
+
+    useEffect(() => {
+        async function fetchToken() {
+            const storedToken = await AsyncStorage.getItem("token");
+
+            if (storedToken) {
+                authCtx.authenticate(storedToken);
+            }
+
+            setIsTryingLogin(false);
+        }
+
+        fetchToken();
+    }, []);
+
+    if (isTryingLogin) {
+        return <AppLoading />;
+    }
+
+    return <Navigation />;
+}
+
 export default function App() {
     return (
         <>
             <StatusBar style="light" />
-            <AuthContextProvider>
-                <Navigation />
-            </AuthContextProvider>
+          <AuthContextProvider>
+        <Root />
+      </AuthContextProvider>
         </>
     );
 }
--- React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/store/auth-context.js
+++ React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/store/auth-context.js
@@ -1,31 +1,35 @@
-import { createContext, useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { createContext, useEffect, useState } from 'react';
 
 export const AuthContext = createContext({
-    token: "",
-    isAuthenticated: false,
-    authenticate: token => {},
-    logout: () => {},
+  token: '',
+  isAuthenticated: false,
+  authenticate: (token) => {},
+  logout: () => {},
 });
 
 function AuthContextProvider({ children }) {
-    const [authToken, setAuthToken] = useState();
+  const [authToken, setAuthToken] = useState();
 
-    function authenticate(token) {
-        setAuthToken(token);
-    }
+  function authenticate(token) {
+    setAuthToken(token);
+    AsyncStorage.setItem('token', token);
+  }
 
-    function logout() {
-        setAuthToken(null);
-    }
+  function logout() {
+    setAuthToken(null);
+    AsyncStorage.removeItem('token');
+  }
 
-    const value = {
-        token: authToken,
-        isAuthenticated: !!authToken,
-        authenticate: authenticate,
-        logout: logout,
-    };
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
+  };
 
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 export default AuthContextProvider;
