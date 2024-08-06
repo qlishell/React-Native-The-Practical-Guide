1c44a88e93aecd3cda15a45e2335ef3eb3147d1f
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-05 21:36:01 -0500

    auth errors handling

--- React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/screens/LoginScreen.js
+++ React Native - The Practical Guide/docs/S11 User Authentication/UserAuth/screens/LoginScreen.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
@@ -9,7 +10,14 @@ function LoginScreen() {
 
     async function loginHandler({ email, password }) {
         setIsAuthenticating(true);
-        await login(email, password);
+        try {
+            await login(email, password);
+        } catch (error) {
+            Alert.alert(
+                "Authentication failed!",
+                "Could not log you in. Please check your credentials or try again later!",
+            );
+        }
         setIsAuthenticating(false);
     }
 
