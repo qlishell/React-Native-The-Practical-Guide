9e23abd815d3bd7cb4c2a6ad62ef7d79e73574ba
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-06 16:38:52 -0500

    allowing map interaction

--- React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/screens/Map.js
+++ React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/screens/Map.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 function Map() {
+    const [selectedLocation, setSelectedLocation] = useState();
+
     const region = {
         latitude: 10.762622, // Vĩ độ của Thành phố Hồ Chí Minh
         longitude: 106.660172, // Kinh độ của Thành phố Hồ Chí Minh
@@ -9,7 +12,26 @@ function Map() {
         longitudeDelta: 0.0421, // Độ phân giải của kinh độ
     };
 
-    return <MapView style={styles.map} initialRegion={region}></MapView>;
+    function selectLocationHandler(event) {
+        const lat = event.nativeEvent.coordinate.latitude;
+        const lng = event.nativeEvent.coordinate.longitude;
+
+        setSelectedLocation({ lat: lat, lng: lng });
+    }
+
+    return (
+        <MapView style={styles.map} initialRegion={region} onPress={selectLocationHandler}>
+            {selectedLocation && (
+                <Marker
+                    title="Picked Location"
+                    coordinate={{
+                        latitude: selectedLocation.lat,
+                        longitude: selectedLocation.lng,
+                    }}
+                />
+            )}
+        </MapView>
+    );
 }
 
 export default Map;
