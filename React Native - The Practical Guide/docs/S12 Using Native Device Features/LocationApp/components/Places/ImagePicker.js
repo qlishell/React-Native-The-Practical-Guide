21d9c70efd3daadca983adca3a903f92fba68f77
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-06 14:08:35 -0500

    showing an image preview

--- React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/components/Places/ImagePicker.js
+++ React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/components/Places/ImagePicker.js
@@ -1,7 +1,12 @@
 import { launchCameraAsync, PermissionStatus, useCameraPermissions } from "expo-image-picker";
-import { Alert, Button, View } from "react-native";
+import { useState } from "react";
+import { Alert, Button, Image, StyleSheet, Text, View } from "react-native";
+
+import { Colors } from "../../constants/colors";
 
 function ImagePicker() {
+    const [pickedImage, setPickedImage] = useState();
+
     const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
 
     async function verifyPermissions() {
@@ -31,15 +36,38 @@ function ImagePicker() {
             aspect: [16, 9],
             quality: 0.5,
         });
-        console.log(image);
+
+        setPickedImage(image.uri);
+    }
+
+    let imagePreview = <Text>No image taken yet.</Text>;
+
+    if (pickedImage) {
+        imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
     }
 
     return (
         <View>
-            <View></View>
+            <View style={styles.imagePreview}>{imagePreview}</View>
             <Button title="Take Image" onPress={takeImageHandler} />
         </View>
     );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+    imagePreview: {
+        width: "100%",
+        height: 200,
+        marginVertical: 8,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: Colors.primary100,
+        borderRadius: 4,
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+    },
+});
