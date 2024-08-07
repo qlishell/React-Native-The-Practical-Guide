f4dc85a3c9852bacaa833ba40b8c6c61df0b0810
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-06 19:31:50 -0500

    fetching place detail data

--- React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/screens/Map.js
+++ React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/screens/Map.js
@@ -4,17 +4,25 @@ import MapView, { Marker } from "react-native-maps";
 
 import IconButton from "../components/UI/IconButton";
 
-function Map({ navigation }) {
-    const [selectedLocation, setSelectedLocation] = useState();
+function Map({ navigation, route }) {
+    const initialLocation = route.params && {
+        lat: route.params.initialLat,
+        lng: route.params.initialLng,
+    };
+
+    const [selectedLocation, setSelectedLocation] = useState(initialLocation);
 
     const region = {
-        latitude: 37.78,
-        longitude: -122.43,
+        latitude: initialLocation ? initialLocation.lat : 37.78,
+        longitude: initialLocation ? initialLocation.lng : -122.43,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
     };
 
     function selectLocationHandler(event) {
+        if (initialLocation) {
+            return;
+        }
         const lat = event.nativeEvent.coordinate.latitude;
         const lng = event.nativeEvent.coordinate.longitude;
 
@@ -34,12 +42,15 @@ function Map({ navigation }) {
     }, [navigation, selectedLocation]);
 
     useLayoutEffect(() => {
+        if (initialLocation) {
+            return;
+        }
         navigation.setOptions({
             headerRight: ({ tintColor }) => (
                 <IconButton icon="save" size={24} color={tintColor} onPress={savePickedLocationHandler} />
             ),
         });
-    }, [navigation, savePickedLocationHandler]);
+    }, [navigation, savePickedLocationHandler, initialLocation]);
 
     return (
         <MapView style={styles.map} initialRegion={region} onPress={selectLocationHandler}>
--- React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/screens/PlaceDetails.js
+++ React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/screens/PlaceDetails.js
@@ -8,7 +8,12 @@ import { fetchPlaceDetails } from "../util/database";
 function PlaceDetails({ route, navigation }) {
     const [fetchedPlace, setFetchedPlace] = useState();
 
-    function showOnMapHandler() {}
+    function showOnMapHandler() {
+        navigation.navigate("Map", {
+            initialLat: fetchedPlace.location.lat,
+            initialLng: fetchedPlace.location.lng,
+        });
+    }
 
     const selectedPlaceId = route.params.placeId;
 
