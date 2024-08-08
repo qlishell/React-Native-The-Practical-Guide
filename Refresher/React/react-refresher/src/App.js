0cd13b0d2b4ffd98c0bb6d3aeae6446b03090c6e
Qliphort Shell <nhoangkiet35@gmail.com>
2024-08-08 00:06:56 -0500

    React Refresher

--- Refresher/React/react-refresher/src/App.js
+++ Refresher/React/react-refresher/src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+import './App.css';
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  const [courseGoals, setCourseGoals] = useState([
+    { id: 'cg1', text: 'Finish the Course' },
+    { id: 'cg2', text: 'Learn all about the Course Main Topic' },
+    { id: 'cg3', text: 'Help other students in the Course Q&A' }
+  ]);
+
+  const addNewGoalHandler = newGoal => {
+    // setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal));
+  };
+
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
+    </div>
+  );
 };
 
 export default App;
