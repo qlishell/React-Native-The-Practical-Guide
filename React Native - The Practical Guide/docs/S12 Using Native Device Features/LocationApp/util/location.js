6e6f45910e0104256e471eb24a7f0150457be9e6
NguyenHHKiet <hknguyen047@gmail.com>
2024-08-09 08:35:27 -0500

    adding a env EXPO_PUBLIC_API_URL variable

--- React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/util/location.js
+++ React Native - The Practical Guide/docs/S12 Using Native Device Features/LocationApp/util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyBi-hikSNBK87aZLUdlbOu6sK-b6UaaR-w";
+const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_API_URL;
 
 export function getMapPreview(lat, lng) {
     const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
